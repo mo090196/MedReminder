77df80340e838035b5e67272c450f254959b5cc6
Mo <abunahiamohammad@gmail.com>
2026-01-17 16:57:30 +0100

    Initial Commit

---
+++ MedReminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MedReminder
+//
+//  Created by TA622 on 17.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MedReminder/MedReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedReminderApp.swift
+//  MedReminder
+//
+//  Created by TA622 on 17.01.26.
+//
+
+import SwiftUI
+
+@main
+struct MedReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
